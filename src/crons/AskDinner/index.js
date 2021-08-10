c125d75b8ce23085520d46c70ac95a738f2b4eea
nozomi-iida <iida19990106@gmail.com>
2021-08-10 22:54:12 +0900

    test

--- src/crons/AskDinner/index.js
+++ src/crons/AskDinner/index.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const utils = require('../../utils');
 
 const AskDinner = () => {
-  cron.schedule('0 0 12 * * *', () => {
+  cron.schedule('0 */1 * * * *', () => {
     const message = {
       "type": "template",
       "altText": "ask today's dinner",
